17fcf8491aadd9f33744ba72f85b188b72dd59f9
APostolit <50382487+APostolit@users.noreply.github.com>
2026-05-15 10:14:28 +0000

    Update cod_editor.py

--- cod_editor.py
+++ cod_editor.py
@@ -408,7 +408,12 @@ def editor(path_file):
                     st.video(imd_path)
                 with tab_2:
                     imd_path = execution_path + '/video/Person_h256.mp4'
-                    st.video(imd_path)                    
+                    st.video(imd_path)
+                    
+            elif path_file == 'pages/glava_7/Listing_8_12_st.py':
+                with st.spinner(text="Ждите, идет обучение модели...", show_time=True):
+                    time.sleep(5)
+                st.write('Процесс обучения модели завершен')        
 
             elif path_file == 'pages/glava_8/Listing_8_14_st.py':
                 st.write('Для данного модуля необходима подключенная камера.')
